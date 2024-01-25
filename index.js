b896ad0a423c90343813c565706e6999a70ec1a9
alan_omam <omamalan@hotmail.it>
2024-01-25 15:16:42 +0100

    imppstazione del programma in modo tale che mi scriva in output nome, cognome e colore. senza pwd

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ let cognome = prompt("Inserire Cognome");
 let colore = prompt("Inserire Colore preferito");
 
 
+document.getElementById("name").innerHTML = nome;
+document.getElementById("surname").innerHTML = cognome;
+document.getElementById("color").innerHTML = colore;
