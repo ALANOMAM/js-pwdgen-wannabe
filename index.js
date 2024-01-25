b0e8d001244072424e5184ce1b08df0a25afce2a
alan_omam <omamalan@hotmail.it>
2024-01-25 15:22:53 +0100

    sistemato la parte tecnica, cioè funziona il programma però lo vogli rendere più estetico.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 let nome = prompt("Inserire Nome");
 let cognome = prompt("Inserire Cognome");
 let colore = prompt("Inserire Colore preferito");
-
+let password = nome+cognome+colore+"23";
 
 document.getElementById("name").innerHTML = nome;
 document.getElementById("surname").innerHTML = cognome;
 document.getElementById("color").innerHTML = colore;
+document.getElementById("pswrd").innerHTML = password;
