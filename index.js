5145645d3c5c5d51826dab538118920c0d7cb621
alan_omam <omamalan@hotmail.it>
2024-01-25 15:40:07 +0100

    FINE PAGINA(con aggiunta di qualche stile)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let cognome = prompt("Inserire Cognome");
 let colore = prompt("Inserire Colore preferito");
 let password = nome+cognome+colore+"23";
 
-document.getElementById("name").innerHTML = nome;
-document.getElementById("surname").innerHTML = cognome;
-document.getElementById("color").innerHTML = colore;
-document.getElementById("pswrd").innerHTML = password;
+document.getElementById("name").innerHTML = " <strong>Il tuo Nome è</strong> : "+nome;
+document.getElementById("surname").innerHTML = "<strong>Il tuo Cognome è</strong> : "+ cognome;
+document.getElementById("color").innerHTML = "<strong>Il tuo Colore Preferito è</strong> : "+ colore;
+document.getElementById("pswrd").innerHTML = "<strong>la tua password quindi risulta essere</strong> : "+ password;
